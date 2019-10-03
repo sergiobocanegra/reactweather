15a6718b761e786536446bf083340f8e3e411f60
stbocanegra <sergio.bocanegra@elektra.com.mx>
2019-10-03 18:39:39 -0500

    Ultima sesion antes de cambiar a ANGULAR 8

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,34 @@
+import transformForecast from './../services/transformForecast';
+
+
 export const SET_CITY = 'SET_CITY';
 
 export const setCity = payload => ({type: SET_CITY, payload});
 
+export const api_key = "0bfd50c494f94587b4ae80baef7cdbaa";
+export const url = "http://api.openweathermap.org/data/2.5/forecast";
+
 export const fetchForecast = payload => {
+
+
+
     return dispatch => {
 
+        const url_forecast = `${url}?q=${city}&appid=${api_key}`;
+
+        // Indicador de Carga de datos!!!
+
+        fetch(url_forecast).then(
+            data => (data.json())
+        ).then(
+            WeatherData => {
+                const forecastData =  transformForecast(WeatherData);
+                console.log(forecastData);
+
+
+                // modificar el estado con el resultado de la  promise (fetch)
+            }
+        )
+            return;
     };
 };
--- src/containers/ForecastExtendedContainer.js
+++ src/containers/ForecastExtendedContainer.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ForecastExtended from './../components/ForecastExtended';
 
-
 class ForecastExtendedContainer extends Component {
     render() {
         return (
